353bf50ecea0fb4f86d2a0107cd73de97beecd4c
arafat2020 <arafatmannan2019@gmail.com>
2020-06-18 17:54:24 +0600

    small divice responsive done

---
+++ js/script.js
@@ -0,0 +1,152 @@
+// banner slider
+$('#banner-part').slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  // venobox for video
+  $('.venobox').venobox();
+  // testimonial slider
+  $('.testimonial-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  // counter-up
+  $('.count-num').counterUp({
+    delay: 10,
+    time: 3000
+});
+// wow^^^^^^^^^^
+new WOW().init();
+
+
+// stcky menubar
+$(window).scroll(function(){
+  var scrollAmount = $(window).scrollTop()
+  if(scrollAmount > 550) {
+    $(".navbar").addClass("fixed")
+  }else {
+    $(".navbar").removeClass("fixed")
+  }
+  if(scrollAmount > 1000) {
+    $(".btop").fadeIn()
+  }else {
+    $(".btop").fadeOut()
+  }
+})
+$(".btop").click(function(){
+  $("html,body").animate({
+    scrollTop:0
+  },1000)
+})
+
+// color tab js
+$("#colorful").colorfulTab();    
+// brand slider
+$('.brand-slider-torun').slick({
+  dots: true,
+  infinite: true,
+  speed: 1500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  prevArrow: '<i class="fa fa-chevron-left prev" aria-hidden="true"></i>',
+  nextArrow: '<i class="fa fa-chevron-right next" aria-hidden="true"></i> ',
+  appendDots: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+        
